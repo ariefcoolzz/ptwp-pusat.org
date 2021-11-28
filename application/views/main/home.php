db2957625c808e4edbb2e0ee38a62e856fde15b9
m-hudri <mh.badilag@gmail.com>
2021-11-28 10:07:23 +0700

    home

--- application/views/main/home.php
+++ application/views/main/home.php
@@ -1,38 +1,225 @@
-<div class="home-lead">
-    <div class="df-logo-initial mg-b-15"><p>df</p></div>
-    <p class="home-text">Responsive Bootstrap 4 Dashboard Template</p>
-    
-    <h6 class="home-headline">Make your dashboard app more professional with this <span>super awesome</span> and <span>premium quality</span> dashboard design template.</h6>
-    
-    <div class="d-flex wd-lg-350">
-        <a href="https://themeforest.net/item/dashforge-responsive-admin-dashboard-template/23725961" class="btn btn-brand-01 btn-uppercase flex-fill">Buy Now - $20</a>
-        <a href="template/classic/dashboard-one.html" class="btn btn-white btn-uppercase flex-fill mg-l-10">Explore Demo</a>
+<header class="navbar navbar-header navbar-header-fixed bd-b-0">
+    <a href="<?php echo base_url('/') ?>" id="mainMenuOpen" class="burger-menu"><i data-feather="menu"></i></a>
+    <div class="navbar-brand">
+        <a href="<?php echo base_url('/') ?>" class="df-logo">PTWP<span>Pusat</span></a>
+    </div><!-- navbar-brand -->
+    <div id="navbarMenu" class="navbar-menu-wrapper">
+        <div class="navbar-menu-header">
+            <a href="<?php echo base_url('/') ?>" class="df-logo">PTWP<span>Pusat</span></a>
+            <a id="mainMenuClose" href=""><i data-feather="x"></i></a>
+        </div><!-- navbar-menu-header -->
+        <ul class="nav navbar-menu">
+            <li class="nav-label pd-l-15 pd-lg-l-25 d-lg-none">Main Navigation</li>
+            <li class="nav-item with-sub">
+                <a href="#" class="nav-link"><i data-feather="pie-chart"></i> Dashboard</a>
+                <ul class="navbar-menu-sub">
+                    <li class="nav-sub-item"><a href="template/classic/dashboard-one.html" class="nav-sub-link"><i data-feather="bar-chart-2"></i>Sales Monitoring</a></li>
+                    <li class="nav-sub-item"><a href="template/classic/dashboard-two.html" class="nav-sub-link"><i data-feather="bar-chart-2"></i>Website Analytics</a></li>
+                    <li class="nav-sub-item"><a href="template/classic/dashboard-three.html" class="nav-sub-link"><i data-feather="bar-chart-2"></i>Cryptocurrency</a></li>
+                    <li class="nav-sub-item"><a href="template/classic/dashboard-four.html" class="nav-sub-link"><i data-feather="bar-chart-2"></i>Helpdesk Management</a></li>
+                </ul>
+            </li>
+            <li class="nav-item with-sub">
+                <a href="#" class="nav-link"><i data-feather="package"></i> Apps</a>
+                <ul class="navbar-menu-sub">
+                    <li class="nav-sub-item"><a href="template/classic/app-calendar.html" class="nav-sub-link"><i data-feather="calendar"></i>Calendar</a></li>
+                    <li class="nav-sub-item"><a href="template/classic/app-chat.html" class="nav-sub-link"><i data-feather="message-square"></i>Chat</a></li>
+                    <li class="nav-sub-item"><a href="template/classic/app-contacts.html" class="nav-sub-link"><i data-feather="users"></i>Contacts</a></li>
+                    <li class="nav-sub-item"><a href="template/classic/app-file-manager.html" class="nav-sub-link"><i data-feather="file-text"></i>File Manager</a></li>
+                    <li class="nav-sub-item"><a href="template/classic/app-mail.html" class="nav-sub-link"><i data-feather="mail"></i>Mail</a></li>
+                </ul>
+            </li>
+            <li class="nav-item with-sub">
+                <a href="" class="nav-link"><i data-feather="layers"></i> Pages</a>
+                <div class="navbar-menu-sub">
+                    <div class="d-lg-flex">
+                        <ul>
+                            <li class="nav-label">Authentication</li>
+                            <li class="nav-sub-item"><a href="template/classic/page-signin.html" class="nav-sub-link"><i data-feather="log-in"></i> Sign In</a></li>
+                            <li class="nav-sub-item"><a href="template/classic/page-signup.html" class="nav-sub-link"><i data-feather="user-plus"></i> Sign Up</a></li>
+                            <li class="nav-sub-item"><a href="template/classic/page-verify.html" class="nav-sub-link"><i data-feather="user-check"></i> Verify Account</a></li>
+                            <li class="nav-sub-item"><a href="template/classic/page-forgot.html" class="nav-sub-link"><i data-feather="shield-off"></i> Forgot Password</a></li>
+                            <li class="nav-label mg-t-20">User Pages</li>
+                            <li class="nav-sub-item"><a href="template/classic/page-profile-view.html" class="nav-sub-link"><i data-feather="user"></i> View Profile</a></li>
+                            <li class="nav-sub-item"><a href="template/classic/page-connections.html" class="nav-sub-link"><i data-feather="user"></i> Connections</a></li>
+                            <li class="nav-sub-item"><a href="template/classic/page-groups.html" class="nav-sub-link"><i data-feather="users"></i> Groups</a></li>
+                            <li class="nav-sub-item"><a href="template/classic/page-events.html" class="nav-sub-link"><i data-feather="calendar"></i> Events</a></li>
+                        </ul>
+                        <ul>
+                            <li class="nav-label">Error Pages</li>
+                            <li class="nav-sub-item"><a href="template/classic/page-404.html" class="nav-sub-link"><i data-feather="file"></i> 404 Page Not Found</a></li>
+                            <li class="nav-sub-item"><a href="template/classic/page-500.html" class="nav-sub-link"><i data-feather="file"></i> 500 Internal Server</a></li>
+                            <li class="nav-sub-item"><a href="template/classic/page-503.html" class="nav-sub-link"><i data-feather="file"></i> 503 Service Unavailable</a></li>
+                            <li class="nav-sub-item"><a href="template/classic/page-505.html" class="nav-sub-link"><i data-feather="file"></i> 505 Forbidden</a></li>
+                            <li class="nav-label mg-t-20">Other Pages</li>
+                            <li class="nav-sub-item"><a href="template/classic/page-timeline.html" class="nav-sub-link"><i data-feather="file-text"></i> Timeline</a></li>
+                            <li class="nav-sub-item"><a href="template/classic/page-pricing.html" class="nav-sub-link"><i data-feather="file-text"></i> Pricing</a></li>
+                            <li class="nav-sub-item"><a href="template/classic/page-help-center.html" class="nav-sub-link"><i data-feather="file-text"></i> Help Center</a></li>
+                            <li class="nav-sub-item"><a href="template/classic/page-invoice.html" class="nav-sub-link"><i data-feather="file-text"></i> Invoice</a></li>
+                        </ul>
+                    </div>
+                </div><!-- nav-sub -->
+            </li>
+            <li class="nav-item"><a href="components/" class="nav-link"><i data-feather="box"></i> Components</a></li>
+            <li class="nav-item"><a href="collections/" class="nav-link"><i data-feather="archive"></i> Collections</a></li>
+        </ul>
+    </div><!-- navbar-menu-wrapper -->
+    <div class="navbar-right">
+        <a href="#" class="btn btn-social"><i class="fab fa-youtube"></i></a>
+        <a href="#" class="btn btn-social"><i class="fab fa-instagram"></i></a>
+        <a href="#" class="btn btn-social"><i class="fab fa-facebook"></i></a>
+    </div><!-- navbar-right -->
+</header><!-- navbar -->
+
+<div class="content-fixed">
+    <div id="slideshow" class="carousel slide carousel-fade" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#slideshow" data-slide-to="0" class="active"></li>
+            <li data-target="#slideshow" data-slide-to="1"></li>
+            <li data-target="#slideshow" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner bg-dark">
+            <div class="carousel-item active">
+                <img src="<?php echo base_url() ?>assets/img/tenis.jpeg" class="d-block ht-600 w-100" alt="...">
+                <div class="carousel-caption">
+                    <h1 class="text-white">PTWP</h1>
+                    <p class="tx-14">Badan Sehat, FIkiran Jernih, Kerja Produktif.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="<?php echo base_url() ?>assets/img/05.png" class="d-block ht-600 w-100" alt="...">
+                <div class="carousel-caption">
+                    <h1 class="text-white">KETUA MAHKAMAH AGUNG RI</h1>
+                    <p class="tx-14">Melakukan Service Pertama Untuk Lapangan Tenis Baru.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="<?php echo base_url() ?>assets/img/06.png" class="d-block ht-600 w-100" alt="...">
+                <div class="carousel-caption">
+                    <h1 class="text-white">KETUA MAHKAMAH AGUNG RI</h1>
+                    <p class="tx-14">Melakukan Service Pertama Untuk Lapangan Tenis Baru.</p>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#slideshow" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"><i data-feather="chevron-left"></i></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#slideshow" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"><i data-feather="chevron-right"></i></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
-    
-    <div class="d-flex tx-20 mg-t-40">
-        <div class="tx-purple"><i class="fab fa-bootstrap"></i></div>
-        <div class="tx-orange pd-l-10"><i class="fab fa-html5"></i></div>
-        <div class="tx-primary pd-l-10"><i class="fab fa-css3-alt"></i></div>
-        <div class="tx-pink pd-l-10"><i class="fab fa-sass"></i></div>
-        <div class="tx-warning pd-l-10"><i class="fab fa-js"></i></div>
-        <div class="tx-danger pd-l-10"><i class="fab fa-npm"></i></div>
-        <div class="tx-danger pd-l-10"><i class="fab fa-gulp"></i></div>
-        <div class="bd-l mg-l-10 mg-sm-l-20 pd-l-10 pd-sm-l-20"></div>
-        <div class="tx-color-03" data-toggle="tooltip" data-title="Ongoing development"><i class="fab fa-angular"></i></div>
-        <div class="tx-color-03 pd-l-10" data-toggle="tooltip" data-title="Coming soon"><i class="fab fa-react"></i></div>
-        <div class="tx-color-03 pd-l-10" data-toggle="tooltip" data-title="Coming soon"><i class="fab fa-vuejs"></i></div>
+</div><!-- content -->
+
+<div class="divider-text">
+    <h4>Berita Terbaru</h4>
+</div>
+<div class="content">
+    <div class="row">
+        <div class="col-sm mg-t-1">
+            <div class="media">
+                <img src="<?php echo base_url() ?>assets/img/gambar.jpg" class="wd-200 rounded mg-r-20" alt="">
+                <div class="media-body">
+                    <h5 class="mg-b-15 tx-inverse">Media heading</h5>
+                    Cras sit amet nibh libero, in gravida nulla...
+                </div>
+            </div>
+        </div>
+        <div class="col-sm mg-t-1">
+            <div class="media">
+                <img src="<?php echo base_url() ?>assets/img/gambar.jpg" class="wd-200 rounded mg-r-20" alt="">
+                <div class="media-body">
+                    <h5 class="mg-b-15 tx-inverse">Media heading</h5>
+                    Cras sit amet nibh libero, in gravida nulla...
+                </div>
+            </div>
+        </div>
+        <div class="col-sm mg-t-1">
+            <div class="media">
+                <img src="<?php echo base_url() ?>assets/img/gambar.jpg" class="wd-200 rounded mg-r-20" alt="">
+                <div class="media-body">
+                    <h5 class="mg-b-15 tx-inverse">Media heading</h5>
+                    Cras sit amet nibh libero, in gravida nulla...
+                </div>
+            </div>
+        </div>
     </div>
-    
-    <div class="tx-12 mg-t-40">
-        <a href="docs.html" class="link-03">Doc<span class="d-none d-sm-inline">umentation</span><span class="d-sm-none">s</span></a>
-        <a href="changelog.html" class="link-03 mg-l-10 mg-sm-l-20">Changelog</a>
-        <a href="https://themeforest.net/licenses/standard" target="_blank" class="link-03 mg-l-10 mg-sm-l-20">Licenses</a>
-        <a href="https://themeforest.net/page/customer_refund_policy" target="_blank" class="link-03 mg-l-10 mg-sm-l-20">Refund Policy</a>
+    <div class="row">
+        <div class="col-sm mg-t-1">
+            <div class="media">
+                <img src="<?php echo base_url() ?>assets/img/gambar.jpg" class="wd-200 rounded mg-r-20" alt="">
+                <div class="media-body">
+                    <h5 class="mg-b-15 tx-inverse">Media heading</h5>
+                    Cras sit amet nibh libero, in gravida nulla...
+                </div>
+            </div>
+        </div>
+        <div class="col-sm mg-t-1">
+            <div class="media">
+                <img src="<?php echo base_url() ?>assets/img/gambar.jpg" class="wd-200 rounded mg-r-20" alt="">
+                <div class="media-body">
+                    <h5 class="mg-b-15 tx-inverse">Media heading</h5>
+                    Cras sit amet nibh libero, in gravida nulla...
+                </div>
+            </div>
+        </div>
+        <div class="col-sm mg-t-1">
+            <div class="media">
+                <img src="<?php echo base_url() ?>assets/img/gambar.jpg" class="wd-200 rounded mg-r-20" alt="">
+                <div class="media-body">
+                    <h5 class="mg-b-15 tx-inverse">Media heading</h5>
+                    Cras sit amet nibh libero, in gravida nulla...
+                </div>
+            </div>
+        </div>
     </div>
 </div>
-<div class="home-slider-img">
-    <div><img src="<?php echo base_url(); ?>assets/img/home-1.png" alt=""></div>
-    <div><img src="<?php echo base_url(); ?>assets/img/home-2.png" alt=""></div>
-    <div><img src="<?php echo base_url(); ?>assets/img/home-2.png" alt=""></div>
+
+<div class="divider-text">
+    <h4>Pengurus PTWP Pusat</h4>
 </div>
-<div class="home-slider-bg-one"></div>
+<div class="content">
+    <div class="row">
+        <div class="col-sm">
+            <figure class="img-caption pos-relative mg-b-0">
+                <img src="<?php echo base_url() ?>assets/img/01.jpg" class="rounded ht-250 w-100" alt="Responsive image">
+                <figcaption class="pos-absolute a-0 wd-100p pd-20 d-flex flex-column justify-content-center bg-white-9 transition-base op-0">
+                    <h6 class="tx-inverse tx-semibold mg-b-20 text-center">Ketua Mahkamah Agung RI</h6>
+                </figcaption>
+            </figure>
+            <h4 class="text-center">Prof. Dr. H. Muhammad Syarifuddin, S.H., M.H.</h4>
+            <h6 class="text-center">KETUA MA RI/PEMBINA PTWP PUSAT</h6>
+        </div>
+        <div class="col-sm">
+            <figure class="img-caption pos-relative mg-b-0">
+                <img src="<?php echo base_url() ?>assets/img/02.jpeg" class="rounded ht-250 w-100" alt="Responsive image">
+                <figcaption class="pos-absolute a-0 wd-100p pd-20 d-flex flex-column justify-content-center bg-white-9 transition-base op-0">
+                    <h6 class="tx-inverse tx-semibold mg-b-20 text-center">Mantan Ketua Mahkamah Agung RI</h6>
+                </figcaption>
+            </figure>
+            <h4 class="text-center">Prof. Dr. H. Muhammad Syarifuddin, S.H., M.H.</h4>
+            <h6 class="text-center">KETUA MA RI/PEMBINA PTWP PUSAT</h6>
+        </div>
+        <div class="col-sm">
+            <figure class="img-caption pos-relative mg-b-0">
+                <img src="<?php echo base_url() ?>assets/img/03.jpg" class="rounded ht-250 w-100" alt="Responsive image">
+                <figcaption class="pos-absolute a-0 wd-100p pd-20 d-flex flex-column justify-content-center bg-white-9 transition-base op-0">
+                    <h6 class="tx-inverse tx-semibold mg-b-20 text-center">Ketua PTWP Pusat</h6>
+                </figcaption>
+            </figure>
+            <h4 class="text-center">Prof. Dr. H. Muhammad Syarifuddin, S.H., M.H.</h4>
+            <h6 class="text-center">KETUA MA RI/PEMBINA PTWP PUSAT</h6>
+        </div>
+        <div class="col-sm">
+            <figure class="img-caption pos-relative mg-b-0">
+                <img src="<?php echo base_url() ?>assets/img/04.jpg" class="rounded ht-250 w-100" alt="Responsive image">
+                <figcaption class="pos-absolute a-0 wd-100p pd-20 d-flex flex-column justify-content-center bg-white-9 transition-base op-0">
+                    <h6 class="tx-inverse tx-semibold mg-b-20 text-center">Ketua Panitia Turnamen PTWP Nasional Tahun 2021</h6>
+                </figcaption>
+            </figure>
+            <h4 class="text-center">Prof. Dr. H. Muhammad Syarifuddin, S.H., M.H.</h4>
+            <h6 class="text-center">KETUA MA RI/PEMBINA PTWP PUSAT</h6>
+        </div>
+    </div>
+</div>
