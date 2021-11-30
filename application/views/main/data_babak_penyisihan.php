21beed0faadbae7c6ab734789cf6044647539c60
Ditbinganis <89172389+dikaandrian@users.noreply.github.com>
2021-11-30 17:38:10 +0700

    update babak penyisihan

---
+++ application/views/main/data_babak_penyisihan.php
@@ -0,0 +1,51 @@
+<div class="content content-fixed">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb breadcrumb-style1 mg-b-0">
+            <li class="breadcrumb-item"><a href="#">Menu</a></li>
+            <li class="breadcrumb-item active" aria-current="page"><?php echo $judul; ?></li>
+        </ol>
+    </nav>
+    <div class="table-responsive">
+        <h2>TURNAMEN TENIS PERORANGAN PIALA KMA 2021</h2>
+        <h4 class="text-center mg-t-20">
+		<select id='kategori'>
+		<?php 
+		$kategori = $this->basic->get_data('master_kategori_pemain'); 
+		foreach($kategori->result_array() as $R){
+			echo "<option value='$R[id_kategori]'>$R[kategori]</option>";
+		}	
+		?>
+		</select>
+		</h4>
+		<div id='konten'></div>
+    </div>
+</div>
+<script>
+$(document).ready(function() {
+	$("#kategori").on("change", function() {
+		// alert();skip();
+		
+		var form_data = new FormData();
+		// form_data.append('id_data_point', id_data_point);
+		$.ajax({
+			url: "<?php echo base_url(); ?>main/data_babak_penyisihan_rekap",
+			type: 'POST',
+			cache: false,
+			contentType: false,
+			processData: false,
+			data: form_data,
+			dataType: 'json',
+			success: function(json) {
+				if (json.status !== true) {
+					alert("Ada Kesalahan... !!!");
+					skip();
+				} else {
+					$("#konten").hide(300);
+					$("#konten").html(json.konten);
+					$("#konten").show(300);
+				}
+			}
+		});
+	});
+});
+</script>
