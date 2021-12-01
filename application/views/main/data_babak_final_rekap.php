65d978f6db1dc4945ead2368c2885358498f270a
m-hudri <mh.badilag@gmail.com>
2021-12-01 23:27:19 +0700

    Update data_babak_final_rekap.php

--- application/views/main/data_babak_final_rekap.php
+++ application/views/main/data_babak_final_rekap.php
@@ -17,47 +17,50 @@ if (COUNT($data->result_array())) {
 			<tr>
 				<td class="align-middle"><?php echo $R['urutan']; ?></td>
 				<td class="align-middle">
-					<ul class="list-group list-group-flush tx-13">
-						<li class="list-group-item d-flex pd-sm-x-20">
-							<div class="img-group">
-								<img src="<?php echo base_url() ?>assets/img/default.png" class="img wd-60 ht-60 rounded-circle" alt="">
-							</div>
-							<div class="pd-20">
-								<p class="tx-medium mg-b-0"><?php echo $R['nama_tim_A']; ?></p>
-							</div>
-							<div class="mg-l-auto d-flex align-self-center">
-								<nav class="nav nav-icon-only">
-									<a class="nav-link"><?php echo $R['set1_tim_A']; ?></a>
-								</nav>
-							</div>
-						</li>
-					</ul>
+					<div class="row">
+						<div class="col-sm">
+							<ul class="list-group list-group-flush tx-13">
+								<li class="list-group-item d-flex pd-sm-x-20">
+									<div class="img-group">
+										<img src="<?php echo base_url() ?>assets/img/default.png" class="img wd-60 ht-60 rounded-circle" alt="">
+									</div>
+									<div class="pd-20">
+										<p class="tx-medium mg-b-0"><?php echo $R['nama_tim_A']; ?></p>
+									</div>
+									<div class="mg-l-auto d-flex align-self-center">
+										<nav class="nav nav-icon-only">
+											<a class="nav-link"><?php echo $R['set1_tim_A']; ?></a>
+										</nav>
+									</div>
+								</li>
+							</ul>
+						</div>
+						<div class="col-sm text-center">
+							<a class="tx-bold">VS</a>
+							<br><?php echo format_tanggal('wddmmmmyyyy', $R['tanggal']); ?>
+							<br><?php echo $R['waktu']; ?>
+							<br>
+							<h6 class="badge badge-primary">Lapangan <?php echo $R['lapangan']; ?></h6>
+						</div>
+						<div class="col-sm">
+							<ul class="list-group list-group-flush tx-13">
+								<li class="list-group-item d-flex pd-sm-x-20">
+									<div class="img-group">
+										<img src="<?php echo base_url() ?>assets/img/default.png" class="img wd-60 ht-60 rounded-circle" alt="">
+									</div>
+									<div class="pd-20">
+										<p class="tx-medium mg-b-0"><?php echo $R['nama_tim_B']; ?></p>
+									</div>
+									<div class="mg-l-auto d-flex align-self-center">
+										<nav class="nav nav-icon-only">
+											<a class="nav-link"><?php echo $R['set1_tim_B']; ?></a>
+										</nav>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
 				</td>
-				<td class="text-center">
-					<a class="tx-bold">VS</a>
-					<br><?php echo format_tanggal('wddmmmmyyyy', $R['tanggal']); ?>
-					<br><?php echo $R['waktu']; ?>
-					<br>
-					<h6 class="badge badge-primary">Lapangan <?php echo $R['lapangan']; ?></h6>
-				</td>
-				<td class="align-middle text-right">
-					<ul class="list-group list-group-flush tx-13">
-						<li class="list-group-item d-flex pd-sm-x-20">
-							<div class="img-group">
-								<img src="<?php echo base_url() ?>assets/img/default.png" class="img wd-60 ht-60 rounded-circle" alt="">
-							</div>
-							<div class="pd-20">
-								<p class="tx-medium mg-b-0"><?php echo $R['nama_tim_B']; ?></p>
-							</div>
-							<div class="mg-l-auto d-flex align-self-center">
-								<nav class="nav nav-icon-only">
-									<a class="nav-link"><?php echo $R['set1_tim_B']; ?></a>
-								</nav>
-							</div>
-						</li>
-					</ul>
-				</td>
-
 			</tr>
 		<?php
 		}
