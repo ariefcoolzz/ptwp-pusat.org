35d2caf5ec7610e7fb56a000cb8de0f656fc6ecd
Arief Kusuma Putra <ariefcoolzz@gmail.com>
2022-10-05 16:32:38 +0700

    Update Coba.php

--- application/controllers/Coba.php
+++ application/controllers/Coba.php
@@ -29,4 +29,71 @@ class Coba extends CI_Controller
 		// $temp_name 	= str_replace("/", " ", $text);
 		echo $temp_name;
 	}
+	public function pecah_pemain()
+	{
+		$data = $this->basic->get_data_where(array('id_kategori' => 5), 'data_pemain');
+		foreach ($data->result_array() as $d) {
+			$tim['id_pemain1'] = $d['id_pemain'];
+			$tim['kategori'] = 5;
+			$this->db->insert('data_tim', $tim);
+			// print_r($pem);
+		}
+		// echo "<pre>";
+		// print_r($data->result_array());
+	}
+	public function pecah_pemain2()
+	{
+		$data = $this->basic->get_data_where(array('id_kategori' => 7), 'data_pemain');
+		$no = 579;
+		foreach ($data->result_array() as $d) {
+			$pem = explode('/', $d['nama']);
+			$tim['id_pemain1'] = $insert['id_pemain'] = $no++;
+			$insert['id_kategori'] = 99;
+			$insert['satker'] = $d['satker'];
+			$insert['nama'] = trim($pem[0]);
+			$this->db->insert('data_pemain', $insert);
+			$tim['id_pemain2'] = $insert['id_pemain'] = $no++;
+			$insert['nama'] = trim($pem[1]);
+			$this->db->insert('data_pemain', $insert);
+			$tim['kategori'] = 7;
+			$this->db->insert('data_tim', $tim);
+			// print_r($pem);
+		}
+		// echo "<pre>";
+		// print_r($data->result_array());
+	}
+	public function pecah_pool()
+	{
+		$char = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+		$data = $this->basic->get_data('data_dami');
+		$no = 0;
+		foreach ($data->result_array() as $d) {
+			if (trim($d['nama']) !== 'NAME') {
+				$pool = strtoupper($char[$no]);
+
+				$pec = explode('(', $d['nama']);
+				$pec2 = explode('/', $pec[0]);
+				$pem1 = trim($pec2[0]);
+				$data = $this->db->query("SELECT *, NAMA_PEMAIN(id_pemain1) AS pemain1, NAMA_PEMAIN(id_pemain2) AS pemain2
+						FROM data_tim AS A
+						WHERE NAMA_PEMAIN(id_pemain1) = '$pem1'")->row_array();
+				echo $pool . '-' . $pem1 . '-' . $data['id_tim'] . '<br>';
+			} else {
+				$no++;
+			}
+		}
+		// echo "<pre>";
+		// print_r($data->result_array());
+	}
+	public function last_query()
+	{
+		print($this->db->last_query());
+	}
+	public function print_session()
+	{
+		echo '<pre>';
+		print_r($_SESSION);
+		echo '</pre>';
+		die();
+	}
 }
