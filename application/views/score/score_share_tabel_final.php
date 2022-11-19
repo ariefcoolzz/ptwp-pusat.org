b34df13218145636b3ac3cfd5d36f08e36a1819a
m-hudri <mh.badilag@gmail.com>
2022-11-20 06:17:51 +0700

    score

--- application/views/score/score_share_tabel_final.php
+++ application/views/score/score_share_tabel_final.php
@@ -1,42 +1,38 @@
-<table class='table bg-white w-100'>
-    <tr class='text-center align-top font-weight-bold'>
-        <td>Semi Final</td>
-        <td class='text-center'>Game</td>
-        <td class='text-center'>Point</td>
-    </tr>
-    <tr class='align-top'>
+<table class='table table-bordered bg-primary tx-white w-100'>
+    <tr>
         <td>
             <div class='d-flex'>
-                <div class="avatar"><img src="<?php echo base_url('assets/img/default.png'); ?>" class="rounded-circle" alt=""></div>
                 <div class="pd-l-10">
                     <p class="tx-medium mg-b-0">Pemain A</p>
-                    <small class="tx-12 tx-color-03 mg-b-0">Tim A</small>
+                    <small class="tx-12  mg-b-0">Pangda A</small>
                 </div>
             </div>
         </td>
         <td id='set1_tim_A' class='text-center'></td>
-        <td class='text-center font-weight-bold' id='point_tim_A'></td>
+        <td class='text-center font-weight-bold bg-success'>0</td>
+        <td class='text-center font-weight-bold bg-warning' id='point_tim_A'></td>
     </tr>
     <tr class='align-top'>
         <td>
             <div class='d-flex'>
-                <div class="avatar"><img src="<?php echo base_url('assets/img/default.png'); ?>" class="rounded-circle" alt=""></div>
                 <div class="pd-l-10">
                     <p class="tx-medium mg-b-0">Pemain B</p>
-                    <small class="tx-12 tx-color-03 mg-b-0">Tim B</small>
+                    <small class="tx-12  mg-b-0">Pangda B</small>
                 </div>
             </div>
         </td>
         <td id='set1_tim_B' class='text-center'></td>
-        <td class='text-center font-weight-bold' id='point_tim_B'></td>
+        <td class='text-center font-weight-bold bg-success'>0</td>
+        <td class='text-center font-weight-bold bg-warning' id='point_tim_B'></td>
     </tr>
 </table>
 <style>
     /* dika jadi tukang cat */
-    .table{
+    .table {
         margin-bottom: 0;
     }
-    .table td{
+
+    .table td {
         line-height: normal;
     }
 </style>
