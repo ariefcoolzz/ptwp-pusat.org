e00440fe95bd3600859fcd0af77b2db981bca441
m-hudri <mh.badilag@gmail.com>
2022-11-20 22:52:32 +0700

    Update score.php

--- application/views/score/score.php
+++ application/views/score/score.php
@@ -1,2 +1,13 @@
-<div style='width:300px;border:1px solid green;'><a href='<?php echo base_url('score/penyisihan'); ?>'>Babak Penyisihan</a></div>
-<div style='width:300px;border:1px solid green;'><a href='<?php echo base_url('score/final'); ?>'>Babak Final</a></div>
+<div class="content content-fixed content-auth-alt">
+    <div class="container ht-100p">
+        <div class="ht-100p d-flex flex-column align-items-center justify-content-center">
+            <div class="wd-150 wd-sm-250 mg-b-30"><img src="../../assets/img/img17.png" class="img-fluid" alt=""></div>
+            <img src="<?php echo base_url('assets/img/maskot.png'); ?>" class="wd-300" alt="">
+            <h4 class="tx-20 tx-sm-24">Score Pertandingan</h4>
+            <div class="tx-13 tx-lg-14 mg-b-40">
+                <a href="<?php echo base_url('score/penyisihan'); ?>" class="btn btn-brand-02 d-inline-flex align-items-center">Babak Penyisihan</a>
+                <a href="<?php echo base_url('score/final'); ?>" class="btn btn-brand-02 d-inline-flex align-items-center mg-l-5">Babak Final</a>
+            </div>
+        </div>
+    </div><!-- container -->
+</div><!-- content -->
