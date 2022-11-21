a0c1f6e436f422ced8750149ee554c6b559e4eda
Dika Andrian <dikaandryan@gmail.com>
2022-11-21 22:28:35 +0700

    live streaming tinggal score salah

--- application/views/main/data_live_streaming.php
+++ application/views/main/data_live_streaming.php
@@ -25,31 +25,62 @@
         <div class="card-body">
             <div class='row'>
                 <?php
-                    $result = $this->Model_main->get_data_where($_POST);
-                    if (!$result->num_rows()) {
+                    $nama_kontingen = "";
+                    $str = ARRAY();
+                    $result = $this->Model_main->model_data_live_streaming();
+                    if ($result->num_rows()) {
                         foreach ($result->result_array() as $R) {
-                            ?>
-                                <div class='col-lg-2'>
-                                    Lapangan 
-                                    xxx xxx xxx xxx
-                                </div>
-                                <div class='col-lg-2'>
-                                    xxx xxx xxx xxx
-                                </div>
-                                <div class='col-lg-2'>
-                                    xxx xxx xxx xxx
-                                </div>
-                                <div class='col-lg-2'>
-                                    xxx xxx xxx xxx
-                                </div>
-                                <div class='col-lg-2'>
-                                    xxx xxx xxx xxx
-                                </div>
-                                <div class='col-lg-2'>
-                                    xxx xxx xxx xxx
-                                </div>
-                            <?php 
+                            $idl = $R['id_lapangan'];
+
+                            IF(!ISSET($str[$idl])) $str[$idl] = "";
+                            IF(!ISSET($nama_kontingen[$idl])) $nama_kontingen[$idl] = "";
+                            IF(!ISSET($set1_tim_A[$idl])) $set1_tim_A[$idl] = 0;
+                            IF(!ISSET($set1_tim_B[$idl])) $set1_tim_B[$idl] = 0;
+                            IF(!ISSET($kemenangan_A[$idl])) $kemenangan_A[$idl] = 0;
+                            IF(!ISSET($kemenangan_B[$idl])) $kemenangan_B[$idl] = 0;
+
+                            $set1_tim_A[$idl] = $R['set1_tim_A'];
+                            $set1_tim_B[$idl] = $R['set1_tim_B'];
+
+                            IF($set1_tim_A[$idl] == '8') $kemenangan_A[$idl]++;
+                            IF($set1_tim_B[$idl] == '8') $kemenangan_B[$idl]++;
+
+                            $nama_kontingen_tim = $R['nama_kontingen_tim_A']." VS ".$R['nama_kontingen_tim_B'];
+
+                            IF($nama_kontingen[$idl] != $nama_kontingen_tim)
+                            {
+                                
+                                $nama_kontingen[$idl] = $R['nama_kontingen_tim_A']." VS ".$R['nama_kontingen_tim_B'];
+                                $str[$idl] .= "<div>$nama_kontingen[$idl] ($kemenangan_A[$idl] - $kemenangan_B[$idl])</div>";
+                            }
+                            $str[$idl] .= "<div class='row'>";
+                            $str[$idl] .= "<div class='col-10'>".nama_singkat($R['nama_pemain_tim_A'])."</div>";
+                            $str[$idl] .= "<div class='col-2'>$R[set1_tim_A]</div>";
+                            $str[$idl] .= "<div class='col-10'>".nama_singkat($R['nama_pemain_tim_B'])."</div>";
+                            $str[$idl] .= "<div class='col-2'>$R[set1_tim_B]</div>";
+                            $str[$idl] .= "</div>";
+                            $str[$idl] .= "<hr>";
+                                
                         }
+
+                        $link = ARRAY();
+                        $result = $this->Model_main->model_data_link_streaming();
+                        if ($result->num_rows()) {
+                            foreach ($result->result_array() as $R) {
+                                $link[$R['id_lapangan']] = $R['link_streaming'];
+                            }
+                        }
+                                
+                        FOR($ke=25;$ke<=30;$ke++)
+                            {
+                                IF(!ISSET($str[$ke])) $str[$ke] = "";
+                                echo "
+                                        <div class='col-lg-2' style='border:1px solid red;'> 
+                                            <span class='btn btn-danger'><a target='_blank' class='text-white' href='$link[$ke]'>Link Youtube</a></span>
+                                            $str[$ke]
+                                        </div>
+                                    ";
+                            }
                     }
                 ?>
             </div>
