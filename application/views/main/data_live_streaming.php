95274e20db0fe134a6512b21b8710c2b3b0ceb90
m-hudri <mh.badilag@gmail.com>
2022-11-22 01:44:42 +0700

    Update data_live_streaming.php

--- application/views/main/data_live_streaming.php
+++ application/views/main/data_live_streaming.php
@@ -10,67 +10,71 @@
         <div class="card-body">
             <div class='row'>
                 <?php
-                    $nama_kontingen = "";
-                    $str = ARRAY();
-                    $result = $this->Model_main->model_data_live_streaming();
+                $nama_kontingen = array();
+                $str = array();
+                $result = $this->Model_main->model_data_live_streaming();
+                if ($result->num_rows()) {
+                    foreach ($result->result_array() as $R) {
+                        $idl = $R['id_lapangan'];
+
+                        if (!isset($str[$idl])) $str[$idl] = "";
+                        if (!isset($nama_kontingen[$idl])) $nama_kontingen[$idl] = "";
+                        if (!isset($set1_tim_A[$idl])) $set1_tim_A[$idl] = 0;
+                        if (!isset($set1_tim_B[$idl])) $set1_tim_B[$idl] = 0;
+                        if (!isset($kemenangan_A[$idl])) $kemenangan_A[$idl] = 0;
+                        if (!isset($kemenangan_B[$idl])) $kemenangan_B[$idl] = 0;
+
+                        $set1_tim_A[$idl] = $R['set1_tim_A'];
+                        $set1_tim_B[$idl] = $R['set1_tim_B'];
+
+                        if ($set1_tim_A[$idl] == '8') $kemenangan_A[$idl]++;
+                        if ($set1_tim_B[$idl] == '8') $kemenangan_B[$idl]++;
+
+                        $nama_kontingen_tim = $R['nama_kontingen_tim_A'] . " VS " . $R['nama_kontingen_tim_B'];
+
+                        if ($nama_kontingen[$idl] != $nama_kontingen_tim) {
+
+                            $nama_kontingen[$idl] = $R['nama_kontingen_tim_A'] . " VS " . $R['nama_kontingen_tim_B'];
+                            // $str[$idl] .= "<div>$nama_kontingen[$idl] ($kemenangan_A[$idl] - $kemenangan_B[$idl])</div>";
+                            $str[$idl] .= "<div class='tx-bold tx-center'>$nama_kontingen[$idl]</div>";
+                        }
+
+                        $str[$idl] .= "<div class='d-flex justify-content-between align-items-center'><div>" . nama_singkat($R['nama_pemain_tim_A']) . "</div>";
+                        $str[$idl] .= "$R[set1_tim_A]</div>";
+                        $str[$idl] .= "<div class='d-flex justify-content-between align-items-center'><div>" . nama_singkat($R['nama_pemain_tim_B']) . "</div>";
+                        $str[$idl] .= "$R[set1_tim_B]</div>";
+                        $str[$idl] .= "<hr>";
+                    }
+
+                    $link = array();
+                    $result = $this->Model_main->model_data_link_streaming();
                     if ($result->num_rows()) {
                         foreach ($result->result_array() as $R) {
-                            $idl = $R['id_lapangan'];
-
-                            IF(!ISSET($str[$idl])) $str[$idl] = "";
-                            IF(!ISSET($nama_kontingen[$idl])) $nama_kontingen[$idl] = "";
-                            IF(!ISSET($set1_tim_A[$idl])) $set1_tim_A[$idl] = 0;
-                            IF(!ISSET($set1_tim_B[$idl])) $set1_tim_B[$idl] = 0;
-                            IF(!ISSET($kemenangan_A[$idl])) $kemenangan_A[$idl] = 0;
-                            IF(!ISSET($kemenangan_B[$idl])) $kemenangan_B[$idl] = 0;
-
-                            $set1_tim_A[$idl] = $R['set1_tim_A'];
-                            $set1_tim_B[$idl] = $R['set1_tim_B'];
-
-                            IF($set1_tim_A[$idl] == '8') $kemenangan_A[$idl]++;
-                            IF($set1_tim_B[$idl] == '8') $kemenangan_B[$idl]++;
-
-                            $nama_kontingen_tim = $R['nama_kontingen_tim_A']." VS ".$R['nama_kontingen_tim_B'];
-
-                            IF($nama_kontingen[$idl] != $nama_kontingen_tim)
-                            {
-                                
-                                $nama_kontingen[$idl] = $R['nama_kontingen_tim_A']." VS ".$R['nama_kontingen_tim_B'];
-                                // $str[$idl] .= "<div>$nama_kontingen[$idl] ($kemenangan_A[$idl] - $kemenangan_B[$idl])</div>";
-                                $str[$idl] .= "<div>$nama_kontingen[$idl]</div>";
-                            }
-                            $str[$idl] .= "<div class='row'>";
-                            $str[$idl] .= "<div class='col-10'>".nama_singkat($R['nama_pemain_tim_A'])."</div>";
-                            $str[$idl] .= "<div class='col-2'>$R[set1_tim_A]</div>";
-                            $str[$idl] .= "<div class='col-10'>".nama_singkat($R['nama_pemain_tim_B'])."</div>";
-                            $str[$idl] .= "<div class='col-2'>$R[set1_tim_B]</div>";
-                            $str[$idl] .= "</div>";
-                            $str[$idl] .= "<hr>";
-                                
+                            $link[$R['id_lapangan']] = $R['link_streaming']; // ini asli dari database
+                            // $link[$R['id_lapangan']] = "https://www.youtube.com/embed/cnqkyfumWco"; // sementara tampilin ini dulu
                         }
+                    }
 
-                        $link = ARRAY();
-                        $result = $this->Model_main->model_data_link_streaming();
-                        if ($result->num_rows()) {
-                            foreach ($result->result_array() as $R) {
-                                $link[$R['id_lapangan']] = $R['link_streaming']; // ini asli dari database
-                                // $link[$R['id_lapangan']] = "https://www.youtube.com/embed/cnqkyfumWco"; // sementara tampilin ini dulu
-                            }
-                        }
-
-                        FOR($ke=25;$ke<=30;$ke++)
-                            {
-                                $i = $ke - 24;
-                                IF(!ISSET($str[$ke])) $str[$ke] = "";
-                                echo "
-                                        <div class='col-lg-2 col-sm-12' style='border:1px solid red;'> 
-                                            Lapangan Tenis UNNES Court $i<br>
-                                            <span data-link='$link[$ke]' class='tonton btn btn-danger'><i data-feather='youtube'></i> Siaran Langsung</span>
-                                            $str[$ke]
+                    for ($ke = 25; $ke <= 30; $ke++) {
+                        $i = $ke - 24;
+                        if (!isset($str[$ke])) $str[$ke] = "";
+                        echo "
+                                        <div class='col-sm-12 col-md-6 col-lg-6 col-xl-2 m-0'> 
+                                            <div class='card'>
+                                                <div class='card-header text-center'>
+                                                    <h6>Lapangan Tenis UNNES Court $i</h6>
+                                                    <button data-link='$link[$ke]' class='tonton btn btn-danger'><i class='fa fa-youtube-play'></i> Siaran Langsung</button>
+                                                </div>
+                                                <div class='card-body px-1'>
+                                                    <div class='overflow-auto ht-300'>
+                                                        $str[$ke]
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     ";
-                            }
                     }
+                }
                 ?>
             </div>
         </div>
@@ -78,10 +82,10 @@
 </div>
 
 <script>
-$(".tonton").on("click", function() {
-    var link = $(this).data('link');
-    $("#judul_popup_streaming").html("SIARAN LANGSUNG LAPANGAN UNNES");
-    $("#isi_popup_streaming").html("<iframe src='" + link + "?autoplay=1' allow='autoplay' width='100%' height='500px' title='YouTube video player' frameborder='0' allow='autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
-    $("#popup_streaming").modal('show');
-});
+    $(".tonton").on("click", function() {
+        var link = $(this).data('link');
+        $("#judul_popup_streaming").html("SIARAN LANGSUNG LAPANGAN UNNES");
+        $("#isi_popup_streaming").html("<iframe src='" + link + "?autoplay=1' allow='autoplay' width='100%' height='500px' title='YouTube video player' frameborder='0' allow='autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
+        $("#popup_streaming").modal('show');
+    });
 </script>
