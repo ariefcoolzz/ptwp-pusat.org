d6665359304464e47463435482f244b0de462b18
Arief Kusuma Putra <ariefcoolzz@gmail.com>
2024-07-08 22:05:46 +0700

    Update Model_admin.php

--- application/models/Model_admin.php
+++ application/models/Model_admin.php
@@ -3,12 +3,15 @@ class Model_admin extends CI_Model
 {
 	function model_get_data_id_nama($keyword = NULL, $veteran = 0)
 	{
+		$id_satker = $_SESSION['id_kontingen'];
+		if($id_satker == '1500') $id_satker = '3';
+		if($id_satker == '1501') $id_satker = '4';
 		$this->db->select("A.id_pegawai AS id");
 		// $this->db->select("CONCAT(\"<span><img sytle='display: inline-block;' class='rounded-circle ht-40 wd-50 pd-x-5' src='//images.weserv.nl/?url=https://sikep.mahkamahagung.go.id/uploads/foto_pegawai/\",A.FotoPegawai,\"&w=200'>\",A.nama,' [',A.nip,']</span>') AS text");
 		$this->db->select("CONCAT(\"<div class='media'><img class='img-thumbnail ht-90 wd-75 mg-r-10' src='//images.weserv.nl/?url=https://sikep.mahkamahagung.go.id/uploads/foto_pegawai/\",A.FotoPegawai,\"&w=200'>\",A.nama,' (',A.umur,')',' <br>',A.nip,' <br>',A.nama_satker,' <br>',NAMA_SATKER('$_SESSION[id_kontingen]'),'</div>') AS text");
 		$this->db->from("data_pegawai_all AS A");
 		$this->db->where("(A.nama_gelar LIKE '%$keyword%' OR A.nip LIKE '%$keyword%')");
-		if (IN_ARRAY($_SESSION['id_panitia'], array(2, 3))) $this->db->where("(A.id_satker = '$_SESSION[id_kontingen]' OR  A.id_satker_parent = '$_SESSION[id_kontingen]')");
+		if (IN_ARRAY($_SESSION['id_panitia'], array(2, 3))) $this->db->where("(A.id_satker = '$id_satker' OR  A.id_satker_parent = '$id_satker')");
 		if ($veteran) $this->db->where("(A.umur >= '60' OR A.id_jabatan = '30')");
 		$this->db->limit("100");
 		$query = $this->db->get();
