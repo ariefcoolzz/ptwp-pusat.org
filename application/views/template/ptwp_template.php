242c173482f1a4f35aec34710237e3a967e3670c
Dika Andrian <89172389+dikaandrian@users.noreply.github.com>
2024-09-14 12:30:25 +0700

    Update ptwp_template.php

--- application/views/template/ptwp_template.php
+++ application/views/template/ptwp_template.php
@@ -125,7 +125,7 @@
                 <li class="nav-sub-item"><a href="<?php echo base_url('main/hasil_drawing_pertandingan'); ?>" class="nav-sub-link"><i data-feather="check-circle"></i> Pedoman Drawing Pertandingan</a></li>
                 <li class="nav-label mg-t-20">Pertandingan</li>
                 <li class="nav-sub-item"><a href="<?php echo base_url('main/data_pool'); ?>" class="nav-sub-link"><i data-feather="check-circle"></i> Data Pool</a></li>
-                <li class="nav-sub-item"><a href="<?php echo base_url('main/data_babak_penyisihan'); ?>" class="nav-sub-link"><i data-feather="check-circle"></i> Babak Penyisihan</a></li>
+                <!-- <li class="nav-sub-item"><a href="<?php echo base_url('main/data_babak_penyisihan'); ?>" class="nav-sub-link"><i data-feather="check-circle"></i> Babak Penyisihan</a></li> -->
                 <!-- <li class="nav-sub-item"><a href="<?php echo base_url('main/data_skema_pertandingan'); ?>" class="nav-sub-link"><i data-feather="check-circle"></i> Skema Pertandingan</a></li> -->
                 <!--<li class="nav-sub-item"><a href="<?php echo base_url('main/data_babak_final/8'); ?>" class="nav-sub-link"><i data-feather="check-circle"></i> Babak Perdelapan Final</a></li>
                 <li class="nav-sub-item"><a href="<?php echo base_url('main/data_babak_final/4'); ?>" class="nav-sub-link"><i data-feather="check-circle"></i> Babak Perempat Final</a></li>
